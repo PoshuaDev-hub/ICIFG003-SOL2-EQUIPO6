7bbe21b7e55a456b462bacad1f247e7043bcc405
vvalenzuelac <vvalenzuelac1@correo.uss.cl>
2026-06-23 19:02:49 -0400

    feat: Interceptor HTTP offline (Issue 9), diagrama MER exportado (Issue 10) y Dockerizacion

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
 import { routes } from './app.routes';
+// 1. Agregamos HTTP_INTERCEPTORS y withInterceptorsFromDi a esta línea
+import { provideHttpClient, withFetch, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
+// 2. Importamos clase ErrorInterceptor
+import { ErrorInterceptor } from './core/interceptors/error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    // 3. Le decimos a HttpClient que use Fetch y que además escuche a nuestro interceptor
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    // 4. Registramos oficialmente el guardia
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
   ]
 };
